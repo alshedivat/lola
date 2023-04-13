817a6a9764c5b7e1f52bb9a288a30ce5aed1a624
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-13 17:46:44 +0800

    Delete lola_dice directory

--- lola_dice/envs/__init__.py
+++
@@ -1,9 +0,0 @@
-from .coin_game import CoinGameVec
-from .matching_pennies import IteratedMatchingPennies
-from .prisoners_dilemma import IteratedPrisonersDilemma
-
-
-# Aliases
-CG = CoinGameVec
-IMP = IteratedMatchingPennies
-IPD = IteratedPrisonersDilemma
